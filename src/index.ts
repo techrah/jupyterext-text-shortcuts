95852f8b6b25de996c060f6c39531a769c6b8583
Ryan Homer <ryan.homer@alumni.ubc.ca>
2019-10-27 10:38:25 -0700

    Implement `autoPad`. Refactor code a bit and add some checks.

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,45 @@
+import { get } from 'lodash/fp';
+import CodeMirror from 'codemirror';
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
-
-import { CodeMirrorEditor } from '@jupyterlab/codemirror';
-import { INotebookTracker } from '@jupyterlab/notebook';
 import { IEditorTracker } from '@jupyterlab/fileeditor';
+import { INotebookTracker } from '@jupyterlab/notebook';
 
 const insertText = (tracker: INotebookTracker) => (args: any) => {
-  let widget = tracker.currentWidget;
-  if (widget) {
-    let text: string = args['text'] as string || '';
-    const editor = widget.content.activeCell.editor as CodeMirrorEditor;
-    const doc = editor.doc;
-    const selectionBefore = doc.getSelection();
-    const from = doc.getCursor("from");
-    const to = doc.getCursor("to");
-    const anchor = doc.getCursor("anchor");
-    const lineLength = doc.getLine(anchor.line).length;
+  const widget = tracker.currentWidget;
+  if (!widget) return;
+
+  const doc = get("content.activeCell.editor.doc", widget) as CodeMirror.Doc;
+  if (!doc) return;
+
+  const selectionBefore = doc.getSelection();
+  const from = doc.getCursor("from");
+  const to = doc.getCursor("to");
+  const anchor = doc.getCursor("anchor");
+  const lineLength = doc.getLine(anchor.line).length;
+
+  const { text, autoPad } = args;
+  const textToInsert = autoPad ? ` ${text} ` : text;
+
+  if (autoPad)
     doc.extendSelection({ ...from, ch: from.ch - 1 }, { ...to, ch: to.ch + 1 });
-    const selectionAfter = doc.getSelection();
-    text = ` ${text} `;
-    let replacementText;
-    if(selectionBefore.length === 0) {
-      if (anchor.ch == 0) replacementText = `${text}${selectionAfter}`;
-      else if (anchor.ch == lineLength) replacementText = `${selectionAfter}${text}`;
-      else replacementText = `${selectionAfter.charAt(0)}${text}${selectionAfter.charAt(1)}`;
-    } else {
-      replacementText = selectionAfter.replace(selectionBefore, text);
-    }
-    replacementText = replacementText.replace(/^[ ]([ ].*)$/, "$1");
-    replacementText = replacementText.replace(/^(.*[ ])[ ]$/, "$1");
-    doc.replaceSelection(replacementText);
+
+  // Determine replacementText by adjust padding based on text selection
+  const selectionAfter = doc.getSelection();
+  let replacementText;
+  if(selectionBefore.length === 0) {
+    if (anchor.ch == 0) replacementText = `${textToInsert}${selectionAfter}`;
+    else if (anchor.ch == lineLength) replacementText = `${selectionAfter}${textToInsert}`;
+    else replacementText = `${selectionAfter.charAt(0)}${textToInsert}${selectionAfter.charAt(1)}`;
+  } else {
+    replacementText = selectionAfter.replace(selectionBefore, textToInsert);
   }
+  replacementText = replacementText.replace(/^[ ]([ ].*)$/, "$1");
+  replacementText = replacementText.replace(/^(.*[ ])[ ]$/, "$1");
+
+  doc.replaceSelection(replacementText);
 };
 
 const handleActivation = (app: JupyterFrontEnd, tracker: INotebookTracker) => {
