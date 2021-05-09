5a15c27820e42258bbe314e58fb95d17c3857038
Ryan Homer <ryan@murage.ca>
2021-05-09 09:36:08 -0700

    Configure Jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+    transformIgnorePatterns: ["/node_modules/(?!(@jupyterlab/.*)/)"],
+};
